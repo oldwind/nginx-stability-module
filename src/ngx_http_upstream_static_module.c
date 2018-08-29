e0384f78f6d5f609ca7dfda5cc97fda8c08b223d
oldwind <534357478@qq.com>
2018-08-29 20:00:07 +0800

    modify

--- src/ngx_http_upstream_static_module.c
+++ src/ngx_http_upstream_static_module.c
@@ -130,7 +130,7 @@ ngx_http_upstream_static_init(ngx_conf_t *cf) {
 
     cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
 
-    h = ngx_array_push(&cmcf->phases[NGX_HTTP_CONTENT_PHASE].handlers);
+    h = ngx_array_push(&cmcf->phases[NGX_HTTP_LOG_PHASE].handlers);
     if (h == NULL) {
         return NGX_ERROR;
     }
