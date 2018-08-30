c702fccf510013586cd8f2b37124981b95738996
oldwind <534357478@qq.com>
2018-08-30 16:49:52 +0800

    modify

--- src/ngx_http_anti_module.c
+++ src/ngx_http_anti_module.c
@@ -18,27 +18,57 @@ static ngx_int_t ngx_http_anti_handler(ngx_http_request_t *r);
 
 
 typedef struct {
-    ngx_int_t          size;
-    ngx_int_t          active_time;
+    ngx_int_t    anti_cache_type;
+    ngx_int_t    anti_cache_key;
+    ngx_int_t    anti_frozen_time;
+    ngx_int_t    anti_use_redis;
+    ngx_int_t    anti_redis_address;
+    ngx_int_t    anti_redis_connect_keeplive;
 } ngx_http_anti_conf_t;
 
 
 static ngx_command_t  ngx_http_anti_module_commands[] = {
-
-    // 将upstream内容静态化的内容大小，超过限制，不做静态化
-    { ngx_string("upstream_static_size"),
+    
+    { ngx_string("anti_cache_type"),
         NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
         ngx_conf_set_size_slot,
         NGX_HTTP_LOC_CONF_OFFSET,
-        offsetof(ngx_http_anti_conf_t, size),
+        offsetof(ngx_http_anti_conf_t, anti_cache_type),
+        NULL },
+    
+    { ngx_string("anti_cache_key"),
+        NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
+        ngx_conf_set_size_slot,
+        NGX_HTTP_LOC_CONF_OFFSET,
+        offsetof(ngx_http_anti_conf_t, anti_cache_key),
         NULL },
 
-    // upstream内容保存有效时间
-    { ngx_string("upstream_static_active_time"),
+    { ngx_string("anti_frozen_time"),
         NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
         ngx_conf_set_size_slot,
         NGX_HTTP_LOC_CONF_OFFSET,
-        offsetof(ngx_http_anti_conf_t, active_time),
+        offsetof(ngx_http_anti_conf_t, anti_frozen_time),
+        NULL },
+    
+    { ngx_string("anti_use_redis"),
+        NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
+        ngx_conf_set_size_slot,
+        NGX_HTTP_LOC_CONF_OFFSET,
+        offsetof(ngx_http_anti_conf_t, anti_use_redis),
+        NULL },
+
+    { ngx_string("anti_redis_address"),
+        NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
+        ngx_conf_set_size_slot,
+        NGX_HTTP_LOC_CONF_OFFSET,
+        offsetof(ngx_http_anti_conf_t, anti_redis_address),
+        NULL },
+
+    { ngx_string("anti_redis_connect_keeplive"),
+        NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
+        ngx_conf_set_size_slot,
+        NGX_HTTP_LOC_CONF_OFFSET,
+        offsetof(ngx_http_anti_conf_t, anti_redis_connect_keeplive),
         NULL },    
 
     ngx_null_command
@@ -79,10 +109,44 @@ static ngx_int_t
 ngx_http_anti_handler(ngx_http_request_t *r){
     
     // 获取配置信息
-    ngx_http_anti_conf_t * uscf;
-    uscf = ngx_http_get_module_loc_conf(r, ngx_http_anti_module);
+    ngx_http_anti_conf_t * ancf;
+    ancf = ngx_http_get_module_loc_conf(r, ngx_http_anti_module);
 
-    // 写文件
+    // 只是针对IPV4的请求访问，UNIX域请求和IPV6不做处理
+    if (r->connection->sockaddr->sa_family != AF_INET) {
+        return NGX_OK;
+    }
+
+    // 获取client ip信息
+    struct sockaddr_in *sin;
+    
+    sin = (struct sockaddr_in *) r->connection->sockaddr;
+
+    ngx_int_t s_addr = sin->sin_addr.s_addr;
+    if (s_addr <= 0) {
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "get client addr error");
+        return NGX_OK;
+    }
+
+    // 获取request uri信息
+    u_char * uri;
+    uri = r->uri.data;
+
+    if (uri == NULL) {
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, 
+            "get request uri is error");
+    }
+
+    // 整理成key
+    // ngx_str_t *key = ;
+
+
+    // 访问共享内存，判断是否超过限制
+    
+    
+    
+    // 写到共享内存
+
     
 
     return NGX_OK;
@@ -91,16 +155,19 @@ ngx_http_anti_handler(ngx_http_request_t *r){
 
 static void * 
 ngx_http_anti_create_loc_conf(ngx_conf_t *cf) {
-    ngx_http_anti_conf_t *uscf;
-    uscf = ngx_pcalloc(cf->pool, sizeof(ngx_http_anti_module));
-    if (uscf == NULL) {
+    ngx_http_anti_conf_t *ancf;
+    ancf = ngx_pcalloc(cf->pool, sizeof(ngx_http_anti_module));
+    if (ancf == NULL) {
         return NULL;
     }
 
-    uscf->active_time = NGX_CONF_UNSET_UINT;
-    uscf->size        = NGX_CONF_UNSET_UINT;
-
-    return uscf;
+    ancf->anti_cache_type       = NGX_CONF_UNSET_UINT;
+    ancf->anti_cache_key        = NGX_CONF_UNSET_UINT;
+    ancf->anti_frozen_time      = NGX_CONF_UNSET_UINT;
+    ancf->anti_use_redis        = NGX_CONF_UNSET_UINT; 
+    ancf->anti_redis_address    = NGX_CONF_UNSET_UINT;
+    ancf->anti_redis_connect_keeplive = NGX_CONF_UNSET_UINT;
+    return ancf;
 }
 
  
@@ -109,8 +176,12 @@ ngx_http_anti_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child) {
     ngx_http_anti_conf_t *prev = parent;
     ngx_http_anti_conf_t *conf = child;
 
-    ngx_conf_merge_value(conf->size, prev->size, 1024);
-    ngx_conf_merge_value(conf->active_time, prev->active_time, 300);
+    ngx_conf_merge_value(conf->anti_cache_type, prev->anti_cache_type, 1);
+    ngx_conf_merge_value(conf->anti_cache_key, prev->anti_cache_key, 12);
+    ngx_conf_merge_value(conf->anti_frozen_time, prev->anti_frozen_time, 300);
+    ngx_conf_merge_value(conf->anti_use_redis, prev->anti_use_redis, 1);
+    ngx_conf_merge_value(conf->anti_redis_address, prev->anti_redis_address, 1);
+    ngx_conf_merge_value(conf->anti_redis_connect_keeplive, prev->anti_redis_connect_keeplive, 1);
 
     return NGX_CONF_OK;
 }
